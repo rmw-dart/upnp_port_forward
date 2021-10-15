c0d7a9e531616721484acad79781a8b864cef7c1
gcxfd <i@rmw.link>
2021-10-15 17:23:12 +0800

    🔶

--- lib/http.dart
+++ lib/http.dart
@@ -2,23 +2,25 @@ import 'dart:io';
 import 'dart:convert';
 import 'dart:async';
 
-Future<String> utf8Read(HttpClientResponse response) async {
+Future<String> read(HttpClientResponse response, Encoding encoding) async {
   final contents = StringBuffer();
-  await for (var data in response.transform(utf8.decoder)) {
+  await for (var data in response.transform(encoding.decoder)) {
     contents.write(data);
   }
   return contents.toString();
 }
 
 extension HttpClientResponseText on HttpClientResponse {
-  Future<String> text() {
-    return utf8Read(this);
+  Future<String> text({Encoding encoding = utf8}) {
+    return read(this, encoding);
   }
 }
 
 class Http {
   late final HttpClient http;
+
   int timeout;
+
   Http({this.timeout = 60}) : http = HttpClient();
 
   Future<HttpClientResponse> get(Uri url) async {
